0e27bb402ef52bfa7c0d2834ce5b6407cc0da0c6
Valentino <v.thorey@gmail.com>
2019-11-20 16:44:23 +0100

    LICENCE file

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 )

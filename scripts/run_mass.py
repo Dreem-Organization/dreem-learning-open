51290c67fb9b0ae2c912d06599d46bb86f94996a
antoine <antoine@dreem.com>
2020-03-02 17:54:25 +0100

    Remove unused code from run_mass.py

--- scripts/run_mass.py
+++ scripts/run_mass.py
@@ -21,26 +21,3 @@ run_experiments(experiments,
                 EXPERIMENTS_DIRECTORY,
                 datasets=datasets, error_tolerant=False)
 
-# format json for dod evaluation
-experiments_folder = {
-    'mass': f"{EXPERIMENTS_DIRECTORY}mass/",
-}
-table = 'base_experiments'
-for dataset in datasets:
-    algo_names = os.listdir(experiments_folder[dataset])
-    for algo_name in algo_names:
-        directories_with_experiments = experiments_folder[dataset] + algo_name + '/'
-        records = os.listdir(directories_with_experiments)
-        for record in records:
-            hypnograms = json.load(
-                open(directories_with_experiments + record + '/hypnograms.json', 'r'))
-            for dodh_id, hypnogram in hypnograms.items():
-
-                if not os.path.exists(
-                        'results/{}/{}/{}/'.format(dataset, table, algo_name)):
-                    os.makedirs(
-                        'results/{}/{}/{}/'.format(dataset, table, algo_name))
-
-                with open('results/{}/{}/{}/'.format(
-                        dataset, table, algo_name) + dodh_id + '.json', 'w') as outfile:
-                    json.dump(hypnogram, outfile, indent=4)
